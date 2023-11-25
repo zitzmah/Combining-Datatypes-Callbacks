ba50511960d051324146013e087ed7834771d811
zitzmah <haleyzitzmann@gmail.com>
2023-11-25 12:39:34 -0700

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -1,3 +1,7 @@
+/****************************************** */
+//Section 2: Combining Data Types
+/****************************************** */
+
 const crayonBox = {
     colors: ["Marshmallow White", "Raspberry Ripple", "Lemon Zest", "Bubblegum Burst", "Forest Fern", "Cotton Candy Cloud"],
 }
@@ -31,3 +35,32 @@ console.log(receipt[1].name)
 const apartmentBuilding = [["Olivia Bennett", "Ethan Hayes", "Isabella Lopez", "Mason Carter", "Ava Mitchell", "Jackson Reed", "Sophia Anderson", "Caleb Parker", "Amelia Turner", "Lucas Morgan"], 5, "brick", 750]
 
 console.log(apartmentBuilding[0][5])
+
+/********************************************** */
+//Combine objects, arrays, and functions more than one level deep
+/********************************************** */
+
+knit = () => {
+    return {
+        item: "scarf",
+        size: "6 ft"
+    }
+}
+console.log(knit().item)
+
+// Create a function crayonSelector that returns an object that has an array (you can reuse your crayonBox object). Log one of the elements of that array.
+crayonSelector = () => {
+    return {
+        color: ["Sunset Orange", "Sky Blue", "Meadow Green"],
+    }
+}
+console.log(crayonSelector().color[1])
+
+// Create a function powerButton that returns a function called options - options should console.log a simple message like select a song. Call that inner function
+powerButton = () =>{
+    return {options: () =>{
+        console.log("select a song")
+    }}
+}
+
+powerButton().options()
