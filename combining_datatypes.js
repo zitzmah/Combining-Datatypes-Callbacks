b17102dd16a9cad2f533b9178891448969defb4a
zitzmah <haleyzitzmann@gmail.com>
2023-11-25 12:19:03 -0700

    Combine objects, arrays, and functions

---
+++ combining_datatypes.js
@@ -0,0 +1,33 @@
+const crayonBox = {
+    colors: ["Marshmallow White", "Raspberry Ripple", "Lemon Zest", "Bubblegum Burst", "Forest Fern", "Cotton Candy Cloud"],
+}
+
+const bottle = {
+    cap: {
+        material: "metal",
+        color: "blue",
+    }
+}
+
+console.log(bottle.cap.color)
+
+const receipt = [
+    cereal = {
+        name: "cheerios",
+        price: 3
+    },
+    dishSoap = {
+        name: "dawn",
+        price: 5
+    },
+    tea = {
+        name: "celestial seasonings",
+        price: 5
+    },
+]
+
+console.log(receipt[1].name)
+
+const apartmentBuilding = [["Olivia Bennett", "Ethan Hayes", "Isabella Lopez", "Mason Carter", "Ava Mitchell", "Jackson Reed", "Sophia Anderson", "Caleb Parker", "Amelia Turner", "Lucas Morgan"], 5, "brick", 750]
+
+console.log(apartmentBuilding[0][5])
